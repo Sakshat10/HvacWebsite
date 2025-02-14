d7dc1f2a5a09bb2ddacd11a1af8873139825106c
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-14 16:17:30 +0530

    fix: update Button import case and add background color to specials button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Button from "@/components/ui/button";
+import Button from "@/components/ui/Button";
 import heroData from "../content/home/hero";
 
 export default function Home() {
   return (
    <div className=" h-screen gap-5 flex justify-center items-center">
     <h1>{heroData.title}</h1>
-    <Button text="VIEW SPECIALS"/>
+    <Button text="VIEW SPECIALS" className="bg-orange-700"/>
     </div>
   );
 }
