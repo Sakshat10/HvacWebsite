e4285f84e4f97fe52f3f81a68a7d41f346155fdf
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-16 01:34:52 +0530

    feat: add navigation links for Navbar component

---
+++ src/content/navbar/navlinks.ts
@@ -0,0 +1,10 @@
+const navlinks = [
+    { label: "Home", href: "/" },
+    { label: "Heating", href: "/heating"},
+    { label: "Cooling", href: "/cooling" },
+    { "label": "About", "href": "/about" },
+    { "label": "Contact Us", "href": "/contact" }
+  ];
+  
+  export default navlinks;
+  
