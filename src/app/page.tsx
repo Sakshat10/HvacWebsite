8b10008cc8e3cfaf5c03fcb2897b19632a5586a6
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-16 00:46:38 +0530

    refactor: remove unused Button component and hero data from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Button from "@/components/ui/Button";
-import heroData from "../content/home/hero";
+
 
 export default function Home() {
   return (
-   <div className=" h-screen gap-5 flex justify-center items-center">
-    <h1>{heroData.title}</h1>
-    <Button text="VIEW SPECIALS" className="bg-primary"/>
+   <div className="">
+ 
     </div>
   );
 }
