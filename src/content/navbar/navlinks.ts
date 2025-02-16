8e2ca3753240eb8887e46fe6e83aa666431ecbef
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-16 23:10:05 +0530

    fix: standardize object property syntax in navlinks

--- src/content/navbar/navlinks.ts
+++ src/content/navbar/navlinks.ts
@@ -2,8 +2,8 @@ const navlinks = [
     { label: "Home", href: "/" },
     { label: "Heating", href: "/heating"},
     { label: "Cooling", href: "/cooling" },
-    { "label": "About", "href": "/about" },
-    { "label": "Contact Us", "href": "/contact" }
+    { label: "About", "href": "/about" },
+    { label: "Contact Us", "href": "/contact" }
   ];
   
   export default navlinks;
