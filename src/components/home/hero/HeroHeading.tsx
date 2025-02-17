8326c90f8ddaf362dcb006cc413d2d7aa6038524
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-17 22:08:25 +0530

    fix: adjust font size and layout in HeroHeading component for improved readability

--- src/components/home/hero/HeroHeading.tsx
+++ src/components/home/hero/HeroHeading.tsx
@@ -4,9 +4,9 @@ import Button from "@/components/ui/Button"
 function HeroHeading() {
   return (
     <div className="flex flex-col gap-6  justify-center">
-        <h1 className="text-bold text-3xl lg:text-5xl text-justify">{heroData.title}</h1>
-        <p className="text-bold text-md text-justify lg:text-2xl">{heroData.description}</p>
-        <h1 className="text-tertiary text-4xl max-lg:text-center lg:text-6xl">{heroData.number}</h1>
+        <h1 className="text-bold text-3xl  text-justify">{heroData.title}</h1>
+        <p className="text-bold text-lg text-justify ">{heroData.description}</p>
+        <h1 className="text-tertiary text-3xl max-lg:text-center ">{heroData.number}</h1>
 
         <Button text="BOOK NOW!" className="bg-tertiary  lg:w-2/3"/>
     </div>
