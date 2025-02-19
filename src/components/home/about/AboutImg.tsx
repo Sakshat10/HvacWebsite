75849252e1f5d366813ca28e81ae30abe65c63eb
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-20 00:35:41 +0530

    feat: update AboutImg component to display a framed image with styling

--- src/components/home/about/AboutImg.tsx
+++ src/components/home/about/AboutImg.tsx
@@ -1,7 +1,9 @@
 function AboutImg() {
   return (
     <div>
-      hello
+      <div className="relative bg-tertiary p-4 rounded-[3rem] max-w-xl mx-auto overflow-hidden">
+  <img src="/images/van.webp" alt="Framed Image" className="rounded-[2.5rem]"/>
+</div>
     </div>
   )
 }
