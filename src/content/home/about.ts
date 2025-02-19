9f5ee71a91361c1b6186055365fee0ff58127e8a
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-20 00:21:13 +0530

    feat: add aboutData for company history and mission in About section

---
+++ src/content/home/about.ts
@@ -0,0 +1,9 @@
+const aboutData = {
+    "title": "5 YEARS OF LEGENDARY HEATING & COOLING SERVICES",
+
+    "para1":"Dean Works has a rich history in the HVAC industry. Providing services like ac repair, installation, replacement, and maintenance in Flowery Branch, GA. Starting in 1954, Joe Henry Gee embarked on his journey, eventually co-founding Willis and Gee Heating & Air with Freeman Willis in 1969. They established a strong foundation of honesty, quality, and customer care. In 1989, Joe’s son, Tim Gee, joined the company, leading to its transformation into Gee Heating & Air. Today, it has become a third-generation family business with Tim’s sons, Joseph and Jacob, joining the team.",
+
+    "para2":"Over the years, Dean Works has expanded its facilities to meet growing customer demands and offer a wider range of mechanical services. While their commitment to superior quality and attentive customer care remains steadfast, they also embrace technological advancements to provide innovative solutions. Based in Gainesville, Georgia, Gee Heating & Air serves residential, commercial, medical, industrial, and refrigeration clients. Their mission, rooted in love, service, and honesty, remains constant: to deliver top-quality service and exceed customer expectations."
+}
+
+export default aboutData;
