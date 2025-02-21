7f45f0db7d2ec19d918e5d3758e2e65cabae93d1
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-21 12:59:07 +0530

    fix: remove duplicate entries in navlinks for improved navigation clarity

--- src/content/navbar/navlinks.ts
+++ src/content/navbar/navlinks.ts
@@ -1,7 +1,13 @@
 const navlinks = [
     { label: "Home", href: "/" },
     { label: "Heating", href: "/heating"},
+    { label: "Heating", href: "/heating"},
+    { label: "Heating", href: "/heating"},
     { label: "Cooling", href: "/cooling" },
+    { label: "Cooling", href: "/cooling" },
+    { label: "Cooling", href: "/cooling" },
+    { label: "About", "href": "/about" },
+    { label: "About", "href": "/about" },
     { label: "About", "href": "/about" },
     { label: "Contact Us", "href": "/contact" }
   ];
