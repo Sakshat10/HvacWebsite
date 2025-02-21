a3920afd04b6c6163bbf373dceb20281e255ab6b
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-21 12:58:53 +0530

    fix: adjust spacing in HeroHeading component for improved readability

--- src/components/home/hero/HeroHeading.tsx
+++ src/components/home/hero/HeroHeading.tsx
@@ -4,7 +4,7 @@ import Button from "@/components/ui/Button"
 function HeroHeading() {
   return (
     <div className="flex flex-col gap-6  justify-center">
-        <h1 className="text-bold text-3xl  text-justify">{heroData.title}</h1>
+        <h1 className="text-bold text-3xl   text-justify">{heroData.title}</h1>
         <p className="text-bold text-lg text-justify ">{heroData.description}</p>
         <h1 className="text-tertiary text-3xl max-lg:text-center ">{heroData.number}</h1>
 
