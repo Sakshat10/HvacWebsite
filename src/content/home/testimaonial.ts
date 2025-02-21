d7f5b9fae1d1bae0e76f067c282f8feb212a6a57
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-21 12:38:19 +0530

    feat: add testimonial data for user feedback on HVAC services

---
+++ src/content/home/testimaonial.ts
@@ -0,0 +1,83 @@
+export const testimonials = [
+  {
+    id: 1,
+    name: "Kristen Belaire",
+    date: "1 February 2025",
+    rating: 5,
+    comment:
+      "I replaced a very old and loud air transfer system with a quiet, efficient new one. The crew was courteous, quiet and efficient. Now I don't have to worry about an emergency replacement.",
+    profileImage: "/images/female.png",
+  },
+  {
+    id: 2,
+    name: "Harvey",
+    date: "10 January 2025",
+    rating: 5,
+    comment:
+      "Great service! The team was very professional, and the installation was seamless. Highly recommended!",
+    profileImage: "/images/man.png",
+  },
+  {
+    id: 3,
+    name: "Emma Johnson",
+    date: "5 February 2025",
+    rating: 4,
+    comment:
+      "Very responsive team. They installed my new HVAC system in a single day. Happy with the results!",
+    profileImage: "/images/female2.png",
+  },
+  {
+    id: 4,
+    name: "David Lee",
+    date: "20 February 2025",
+    rating: 5,
+    comment:
+      "Excellent customer service! The team was knowledgeable and helped me choose the perfect system for my home.",
+    profileImage: "/images/man4.png",
+  },
+  {
+    id: 5,
+    name: "Sophia Patel",
+    date: "15 January 2025",
+    rating: 4,
+    comment:
+      "I'm very satisfied with the installation of my new air conditioning system. The crew was efficient and cleaned up after themselves.",
+    profileImage: "/images/female3.png",
+  },
+  {
+    id: 6,
+    name: "Michael Brown",
+    date: "25 February 2025",
+    rating: 5,
+    comment:
+      "The team did an outstanding job installing my new HVAC system. They were professional, courteous, and finished the job quickly.",
+    profileImage: "/images/man2.png",
+  },
+  {
+    id: 7,
+    name: "Rachel Kim",
+    date: "10 February 2025",
+    rating: 4,
+    comment:
+      "I was impressed with the quality of work and the price. I would definitely recommend this company to my friends and family.",
+    profileImage: "/images/female4.png",
+  },
+  {
+    id: 8,
+    name: "Oliver Davis",
+    date: "28 February 2025",
+    rating: 5,
+    comment:
+      "The team was very helpful and explained everything clearly. I'm very happy with my new HVAC system.",
+    profileImage: "/images/man3.png",
+  },
+  {
+    id: 9,
+    name: "Ethan Hall",
+    date: "22 February 2025",
+    rating: 5,
+    comment:
+      "I was very impressed with the professionalism and expertise of the team. They did a great job installing my new air conditioning system.",
+    profileImage: "/images/man5.png",
+  },
+];
