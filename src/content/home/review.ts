9be692ea987b2c7cf5f9a28e71daf4f8ab3265ec
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-21 12:42:29 +0530

    feat: update review data to include contractor name and trust index reviews

--- src/content/home/review.ts
+++ src/content/home/review.ts
@@ -1,7 +1,8 @@
 const review = {
     "heading" : "5-Star Rated HVAC Contractor in Boston, MA",
-    "subheading": "Get the best 5-star rated HVAC service in Boston, MA with Gee Heating & Air. Contact us today!",
-    "title": "Dean Works Heating & Air"
+    "subheading": "Get the best 5-star rated HVAC service in Boston, MA with Dean Works Heating & Air. Contact us today!",
+    "title": "Dean Works Heating & Air",
+    "value": "128 Trustindex reviews"
 
 }
 
