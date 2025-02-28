ba611f2c5607dd08a2b6cff4698943d0f4d500ee
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-28 15:42:08 +0530

    feat: add service data for Same-Day AC Repair services in Flowery Branch

---
+++ src/content/home/serviceData.ts
@@ -0,0 +1,9 @@
+export const serviceData = {
+    title: "Same-Day AC Repair, Installation, Replacement, and Maintenance Service in Flowery Branch",
+    description: `The hot, humid weather can ruin your heating and cooling systems if they’re not properly taken care of. That’s why scheduling regular maintenance is important to protecting your heating and cooling system in Flowery Branch, GA, and saving money in the long run.
+    
+    But if the extreme temperatures and humidity have already caused damage, a Gee Heating & Air technician can help you find a solution for your home quickly. Have you noticed unusual sounds, air that’s not heating or cooling properly, or skyrocketing utility bills? These could be signs of a problem that needs fixing.`,
+    buttonText: "BOOK NOW",
+    imageUrl: "/images/man6.webp", 
+  };
+  
