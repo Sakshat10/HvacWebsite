5f6bfe9f987bc59090ed6878887b3a9af5b8e702
sakshat kumar <Sakshatkumar007@gmail.com>
2025-02-28 14:14:22 +0530

    fix: change background color from primary to dark for improved visual contrast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
    <Testimonial/>
 
 {/* ******************************Section 4  ******************************** */}
-    <div className="bg-primary flex flex-wrap items-center justify-center gap-4 lg:gap-10 px-3 py-5">
+    <div className="bg-dark flex flex-wrap items-center justify-center gap-4 lg:gap-10 px-3 py-5">
       <h1 className="font-bold text-xl max-lg:text-center md:text-2xl">{estimate.title}</h1>
       <Button text="FREE ESTIMATE" className="bg-tertiary lg:w-1/5"/>
     </div>
