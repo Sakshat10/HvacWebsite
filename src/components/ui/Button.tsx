162a77c97ed75fce2a8fdde59e581068f8e50c9d
sakshat kumar <Sakshatkumar007@gmail.com>
2025-03-02 16:38:07 +0530

    feat: enhance Button component to scroll to form section on click

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 
 interface ButtonProps {
@@ -7,9 +8,22 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ text, onClick, className }) => {
+  const handleClick = () => {
+    // Scroll to the "form" section
+    const formSection = document.getElementById("form");
+    if (formSection) {
+      formSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Execute additional custom onClick function if provided
+    if (onClick) {
+      onClick();
+    }
+  };
+
   return (
     <button
-      onClick={onClick}
+      onClick={handleClick}
       className={`relative w-full px-6 py-4 text-white font-bold text-2xl rounded-full shadow-[0_6px_0_#5B3E75,0_8px_12px_rgba(91,62,117,0.4)] transition-all duration-200 hover:shadow-[0_2px_0_#5B3E75,0_4px_8px_rgba(91,62,117,0.2)] hover:translate-y-1 active:shadow-none active:translate-y-2 ${className}`}
     >
       {text}
