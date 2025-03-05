652cd437e58545f9eca933a1ceb8f989c0f8ec02
sakshat kumar <Sakshatkumar007@gmail.com>
2025-03-05 11:48:58 +0530

    feat: add button text to emergency data for improved user interaction

--- src/content/navbar/emergency.ts
+++ src/content/navbar/emergency.ts
@@ -1,6 +1,7 @@
 const emergency = {
     "heading":"24/7 Emergency Service",
-    "subheading":"(770) 232-6523"
+    "subheading":"(770) 232-6523",
+    "ButtonText": "Book now!"
 }
 
 export default emergency;
