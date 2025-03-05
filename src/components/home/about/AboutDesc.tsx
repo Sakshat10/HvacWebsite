2d8ae819c6453f414e0371238d8067746ca902ad
sakshat kumar <Sakshatkumar007@gmail.com>
2025-03-05 11:46:12 +0530

    feat: update Button component text to use dynamic values from estimate and aboutData

--- src/components/home/about/AboutDesc.tsx
+++ src/components/home/about/AboutDesc.tsx
@@ -6,7 +6,7 @@ function AboutDesc() {
     <div className="flex flex-col gap-5">
       <h1 className="text-xl font-bold">about us</h1>
       <h1 className="text-tertiary font-bold text-xl lg:text-4xl">{aboutData.title}</h1>
-      <Button text="learn more" className="bg-tertiary lg:w-1/3" />
+      <Button text={aboutData.ButtonText} className="bg-tertiary lg:w-1/3" />
       <p className="text-justify">{aboutData.para1}</p>
       <p className="text-justify">{aboutData.para2}</p>
     </div>
