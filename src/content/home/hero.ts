c3f4f5dea9f3be07949055d3a2673e4686419b08
sakshat kumar <Sakshatkumar007@gmail.com>
2025-03-05 11:48:53 +0530

    feat: add button text to heroData for improved user interaction

--- src/content/home/hero.ts
+++ src/content/home/hero.ts
@@ -1,7 +1,8 @@
 const heroData = {
     "title" : "The #1 HVAC Contractor in Boston, MA with 30+ Years Of Experience",
     "description": "Finding an HVAC Contractor in Flowery Branch, GA just got easier. Gee Heating & Air is here to help! Get the 5-star treatment and bring comfort back to home for your family! With 30 years of experience you can rest assured we can provide the best ac repair, installation, replacement, and maintenance service available! contact us today to get a free estimate before getting started.",
-    "number":"(770) 232-6523"
+    "number":"(770) 232-6523",
+    "ButtonText": "book now!"
 }
 
 export default heroData;
