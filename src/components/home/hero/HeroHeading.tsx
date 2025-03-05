ca88ac6f452136633663795ff51dad97523126d4
sakshat kumar <Sakshatkumar007@gmail.com>
2025-03-05 12:32:02 +0530

    feat: add animation to HeroHeading component for enhanced user experience

--- src/components/home/hero/HeroHeading.tsx
+++ src/components/home/hero/HeroHeading.tsx
@@ -1,16 +1,24 @@
-import heroData from "@/content/home/hero"
-import Button from "@/components/ui/Button"
+"use client";
+
+import { motion } from "framer-motion";
+import heroData from "@/content/home/hero";
+import Button from "@/components/ui/Button";
 
 function HeroHeading() {
   return (
-    <div className="flex flex-col gap-6  justify-center">
-        <h1 className="text-bold text-3xl   text-justify">{heroData.title}</h1>
-        <p className="text-bold text-lg text-justify ">{heroData.description}</p>
-        <h1 className="text-tertiary text-3xl max-lg:text-center ">{heroData.number}</h1>
-
-        <Button text={heroData.ButtonText} className="bg-tertiary  lg:w-2/3" />
-    </div>
-  )
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut", delay: 0.2}} 
+      viewport={{ once: true, amount: 0.2 }} 
+      className="flex flex-col gap-6 justify-center"
+    >
+      <h1 className="text-bold text-3xl text-justify">{heroData.title}</h1>
+      <p className="text-bold text-lg text-justify">{heroData.description}</p>
+      <h1 className="text-tertiary text-3xl max-lg:text-center">{heroData.number}</h1>
+      <Button text={heroData.ButtonText} className="bg-tertiary lg:w-2/3" />
+    </motion.div>
+  );
 }
 
-export default HeroHeading
+export default HeroHeading;
